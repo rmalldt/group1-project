d9d51c3af462cfd5b9ffa189afdd429bac9636cb
rmalldt <rmall.dt@gmail.com>
2025-05-16 11:17:02 +0100

    Add docker-compose

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,23 +1,34 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const db =
-  process.env.NODE_ENV === 'dev' || 'test'
-    ? new Pool({
-        connectionString: process.env.SUPABASE_URL,
-      })
-    : new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-      });
+let db;
 
-console.log(
-  `DB connection established for ${
-    process.env.NODE_ENV ?? 'production'
-  } environment.`
-);
+function setDb() {
+  if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+    db = new Pool({
+      connectionString: process.env.SUPABASE_URL,
+    });
+    console.log(`DB connection established for ${process.env.NODE_ENV}`);
+  } else {
+    db = new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT || 5432,
+    });
+
+    db.connect(err => {
+      if (err) {
+        console.log('Database connection error', err.stack);
+      } else {
+        console.log('Database connected: ', db);
+      }
+    });
+    console.log('DB connection established for production');
+  }
+}
+
+setDb();
 
 module.exports = db;
