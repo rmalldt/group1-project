2750641b2e4e2540e362bf29f8bd8f18f9eba37c
rmalldt <rmall.dt@gmail.com>
2025-05-19 09:52:21 +0100

    Fix db connect environments

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,23 +1,34 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const db =
-  process.env.NODE_ENV === 'dev' || 'test'
-    ? new Pool({
+let db;
+
+function setDb() {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+    case 'test':
+      db = new Pool({
         connectionString: process.env.SUPABASE_URL,
-      })
-    : new Pool({
+      });
+      break;
+    case 'production':
+      db = new Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        port: process.env.DB_PORT || 5432,
       });
+      break;
 
-console.log(
-  `DB connection established for ${
-    process.env.NODE_ENV ?? 'production'
-  } environment.`
-);
+    default:
+      throw new Error(
+        'Database connection error: process.env.NODE_ENV not defined!'
+      );
+  }
+  console.log(`DB connection established for ${process.env.NODE_ENV}`);
+}
+
+setDb();
 
 module.exports = db;
