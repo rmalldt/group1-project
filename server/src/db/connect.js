76aa66bbd287d177005c96907042e3f00eed9077
rmalldt <rmall.dt@gmail.com>
2025-05-20 01:52:08 +0100

    Add kubernetes deployment and service yaml configurations for api and database with corresponding configmap files with secrets removed

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -17,7 +17,7 @@ function setDb() {
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT || 5432,
+        port: parseInt(process.env.DB_PORT),
       });
       break;
 
